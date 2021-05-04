fb9a99782bb4d00d70c6849e049d3b825c430666
anjaspratamap <anjaspp2@gmail.com>
2021-05-04 13:59:15 +0700

    tambah file laravel

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright by <a href="#">@AdminLTE.io</a></strong> Edit by @anjaspratama All rights reserved.
+  </footer>
